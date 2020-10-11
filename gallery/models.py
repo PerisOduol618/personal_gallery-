a5eb2b1eae005b2e3d78dc8a93e244eaa407a774
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-11 13:54:09 +0300

    Add  location and category to y=the db

--- gallery/models.py
+++ gallery/models.py
@@ -3,20 +3,33 @@ from django.db import models
 class Photos(models.Model):
     photo_name =models.CharField(max_length = 30)
     photo_description = models.TextField()
-    Photo = models.ImageField(upload_to = 'images/',default='image.jpg')
-    photo_location = models.ForeignKey('Location',default = '')
-    photo_category = models.ForeignKey('Category',default='')
+    photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    Photo = models.ImageField(upload_to = 'images/', default='image.jpg')
 
     def __str__(self):
         return self.photo_name
-
-
+    
+    def save_image(self):
+        self.save()
+    
+    
 class Location(models.Model):
     location_name = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.location_name
 
+    def save_location_name(self):
+        self.save()
+
+
+    # def delete_location_name(self):
+    #     self.delete()
+
+    
+
+ 
 
 class Category(models.Model):
     category_name = models.CharField(max_length = 30)
@@ -24,3 +37,4 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
