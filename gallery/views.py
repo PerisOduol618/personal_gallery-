5db47f16bec1707a59014158a321e55e9d80de21
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-12 02:14:59 +0300

    import Photos

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from .models import Photos
 
 
 # Create your views here.
@@ -7,7 +8,9 @@ def home(request):
     return render(request,'home.html')
 
 
-# def gallery(request):
+def photos(request):
+    photo = Photos.objects.all()
+    return render(request, 'photo.html', {"photo" : photo})
 
 
 
