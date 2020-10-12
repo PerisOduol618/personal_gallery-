0d2f65b878a65754fe8588145695ca664d2125ea
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-12 22:45:39 +0300

    style the landing page
    Add  final styling to the whole app

--- gallery/static/js/script.js
+++ gallery/static/js/script.js
@@ -1,7 +1,8 @@
-function myFunction() {
+function myFunction(){
   var copyText = document.getElementById("myInput");
   copyText.select();
   copyText.setSelectionRange(0, 99999)
   document.execCommand("copy");
+  alert("WAAAHHH")
   alert("Copied the text: " + copyText.value);
 }
